a3704ef470627cd9d1374cbefc8b9e9b24da8619
sepehrshakibi <104255394+sepehr757@users.noreply.github.com>
2022-06-26 00:11:22 +0430

    home work 2

---
+++ hw_2/bin/home_2.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  var Friends_name = [];
+  Friends_name.add("Sepehr");
+  Friends_name.add("Arash");
+  Friends_name.add("Soheil");
+  Friends_name.add("Amir");
+  print(Friends_name);
+}
