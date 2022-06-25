6ddda30cbcd1181dacb8ae3f6cc35f969c1fcddc
sepehrshakibi <104255394+sepehr757@users.noreply.github.com>
2022-06-26 00:09:58 +0430

    home work1

---
+++ hw_1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Sepehr Shakibi
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Home Work1"),
+      ),
+    ));
